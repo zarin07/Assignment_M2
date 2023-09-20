e09061a2c5516336488894e7c33b7b8e8f450df0
Zarin Tasnim <zarintasnim271@gmail.com>
2023-09-20 21:41:20 +0600

    assignment two done

---
+++ task_1.php
@@ -0,0 +1,31 @@
+<?php
+//for loop
+
+// for($i=1;$i<=20;$i++){
+//     if($i%2 == 0){
+//         echo $i .",";
+//     };
+// }
+
+//while loop
+
+// $i =1;
+// while($i<=20){
+//     if($i%2 == 0){
+//         echo $i. ",";
+//     }
+    
+//     $i++;
+// }
+
+//do while loop
+
+$i = 1;
+do{
+    if ($i%2 == 0){
+        echo $i;
+    };
+    
+    
+    $i++;
+}while($i<=20);
---
+++ task_4.php
@@ -0,0 +1,17 @@
+<?php
+function Fibonacci($n){
+    $num1 = 0;
+    $num2 = 1;
+    $counter = 0;
+    while($counter < $n){
+        echo ' '.$num1 ;
+        $num3 = $num2 + $num1;
+        $num1 = $num2;
+        $num2 = $num3;
+        
+        $counter = $counter + 1;
+    
+    }
+}
+
+Fibonacci(15);
